3ea9699d49d70495b7ac00bf96e0a0aec56e5b94
kyunooh <chm073@gmail.com>
2015-07-13 02:24:47 +0900

    Applying lombok domain

--- api/src/main/java/com/goree/api/domain/Group.java
+++ api/src/main/java/com/goree/api/domain/Group.java
@@ -2,51 +2,14 @@ package com.goree.api.domain;
 
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class Group {
     private int id;
     private Member leader;
     private String name;
     private String description;
     private List<Member> members;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public Member getLeader() {
-        return leader;
-    }
-    public void setLeader(Member leader) {
-        this.leader = leader;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public List<Member> getMembers() {
-        return members;
-    }
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-    
-    @Override
-    public boolean equals(Object obj){
-        if (!(obj instanceof Group)) 
-            return false;
-        
-        Group group = (Group) obj;
-        return this.getId() == group.getId();
-    }
     
 }
--- api/src/main/java/com/goree/api/domain/Member.java
+++ api/src/main/java/com/goree/api/domain/Member.java
@@ -1,5 +1,8 @@
 package com.goree.api.domain;
 
+import lombok.Data;
+
+@Data
 public class Member {
     private int id;
     private String email;
@@ -16,84 +19,5 @@ public class Member {
         M, F
     }
     
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getFacebook() {
-        return facebook;
-    }
-
-    public void setFacebook(String facebook) {
-        this.facebook = facebook;
-    }
-
-    public String getTwitter() {
-        return twitter;
-    }
-
-    public void setTwitter(String twitter) {
-        this.twitter = twitter;
-    }
-
-    public String getKakaotalk() {
-        return kakaotalk;
-    }
-
-    public void setKakaotalk(String kakaotalk) {
-        this.kakaotalk = kakaotalk;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
     
 }
