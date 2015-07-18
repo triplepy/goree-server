7a61576bddb294f10fd599d2095c8166991a7505
WonyoungJu <getogrand@gmail.com>
2015-07-18 14:40:45 +0900

    Improvement design of DBUnitOperator.

--- api/src/test/java/com/goree/api/util/DBUnitOperator.java
+++ api/src/test/java/com/goree/api/util/DBUnitOperator.java
@@ -2,28 +2,54 @@ package com.goree.api.util;
 
 import java.sql.SQLException;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
+
 import org.dbunit.DataSourceDatabaseTester;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.operation.DatabaseOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DBUnitOperator {
-    private static BasicDataSource dataSource;
+    private IDatabaseTester tester;
     
-    public static void cleanInsert(IDataSet dataset) {
-        dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("net.sf.log4jdbc.DriverSpy");
-        dataSource.setUrl("jdbc:log4jdbc:mysql://localhost:3306/goree");
-        dataSource.setUsername("goree");
-        dataSource.setPassword("goree!@#$");
-        
-        IDatabaseTester tester = new DataSourceDatabaseTester(dataSource);
+    @Autowired
+    public DBUnitOperator(DataSource dataSource) {
+        this.tester = new DataSourceDatabaseTester(dataSource); 
+    }
+    
+    public void cleanInsert(IDataSet dataset) {
+        execute(DatabaseOperation.CLEAN_INSERT, dataset);
+    }
+    
+    public void delete(IDataSet dataset) {
+        execute(DatabaseOperation.DELETE, dataset);
+    }
+    
+    public void deleteAll(IDataSet dataset) {
+        execute(DatabaseOperation.DELETE_ALL, dataset);
+    }
+    
+    public void insert(IDataSet dataset) {
+        execute(DatabaseOperation.INSERT, dataset);
+    }
+    
+    public void refresh(IDataSet dataset) {
+        execute(DatabaseOperation.REFRESH, dataset);
+    }
+    
+    public void truncateTable(IDataSet dataset) {
+        execute(DatabaseOperation.TRUNCATE_TABLE, dataset);
+    }
+    
+    private void execute(DatabaseOperation operation, IDataSet dataset) {
         IDatabaseConnection conn = null;
         try {
             conn = tester.getConnection();
-            DatabaseOperation.CLEAN_INSERT.execute(conn, dataset);
+            operation.execute(conn, dataset);
         } catch (Exception e) {
             throw new RuntimeException();
         } finally {
@@ -35,6 +61,5 @@ public class DBUnitOperator {
                 throw new RuntimeException(e);
             }
         }
-        
     }
 }
--- api/src/test/java/com/goree/api/util/DBUnitOperatorTest.java
+++ api/src/test/java/com/goree/api/util/DBUnitOperatorTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.goree.api.Application;
+import com.goree.api.domain.Group;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,32 +24,33 @@ import com.goree.api.Application;
 public class DBUnitOperatorTest {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private DBUnitOperator opeartor;
     
     private Connection conn;
     
     @Before
     public void setUp() throws Exception {
         conn = dataSource.getConnection();
-        
         String createSql = "CREATE TABLE dbunit_test ( id int primary key auto_increment, name varchar(20))";
-        
         conn.createStatement().executeUpdate(createSql);
-        
     }
     
     @Test
-    public void test() throws Exception {
+    public void cleanInsert() throws Exception {
         IDataSet dataset = IDataSetFactory.fromXml("src/test/resources/testdataset/dbunitoperator.xml");
-        DBUnitOperator.cleanInsert(dataset);
+        opeartor.cleanInsert(dataset);
 
-        
         String selectSql = "SELECT * FROM dbunit_test";
-        
         ResultSet resultSet = conn.prepareStatement(selectSql).executeQuery();
+
+        Group expected = new Group();
+        expected.setId(1);
+        expected.setName("name");
         
         if (resultSet.next()){
-            Assert.assertEquals(resultSet.getInt("id"), 1);
-            Assert.assertEquals(resultSet.getString("name"), "Hello");
+            Assert.assertEquals(expected.getId(), resultSet.getInt("id"));
+            Assert.assertEquals(expected.getName(), resultSet.getString("name"));
         } else { 
             Assert.fail();
         }
