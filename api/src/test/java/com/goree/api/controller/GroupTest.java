ea33596d3c736b14662de205a85fcd20d59c3fd0
kyunooh <chm073@gmail.com>
2015-07-18 13:22:28 +0900

    Refactoring setUp and sorting code

--- api/src/test/java/com/goree/api/controller/GroupTest.java
+++ api/src/test/java/com/goree/api/controller/GroupTest.java
@@ -1,16 +1,11 @@
 package com.goree.api.controller;
 
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.dbunit.DataSourceDatabaseTester;
-import org.dbunit.IDatabaseTester;
-import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
-import org.dbunit.operation.DatabaseOperation;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,84 +17,79 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.goree.api.Application;
 import com.goree.api.domain.Group;
 import com.goree.api.domain.Member;
-import com.goree.api.service.MemberService;
+import com.goree.api.mapper.MemberMapper;
 import com.goree.api.util.DBUnitOperator;
 import com.goree.api.util.IDataSetFactory;
 
+import scala.annotation.meta.getter;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes={Application.class})
+@SpringApplicationConfiguration(classes = { Application.class })
 public class GroupTest {
-    @Autowired
-    private GroupController groupController;
-    @Autowired
-    private MemberService memberService;
-    
-    @Autowired
-    private DataSource dataSource;
-    
-    
-    @Before
-    public void setUp() {
-    	DBUnitOperator.setDataSource(dataSource);
-    	IDataSet dataset = IDataSetFactory.fromXml("src/test/resources/testdataset/findGroupsJoined_setup.xml");
-    	DBUnitOperator.cleanInsert(dataset);
-    }
-    
-    
-    @Test
-    public void findGroupsJoined() throws Exception {
-        
-        Member member = new Member();
-        member.setId(1);
-        List<Group> joinedGroups = groupController.findGroupsJoined(member);
-        Assert.assertTrue(joinedGroups.size() == 1);
-        Assert.assertTrue(joinedGroups.get(0).getId() == 1 );
-    }
-    
-    @Test
-    public void createGroup() {
-        List<Member> memberList = memberService.findMemberAll();
-        Member leader = null;
-        if(memberList != null && !memberList.isEmpty() ){
-            leader = memberList.get(0);
-        } else {
-            
-            Assert.fail("memberList is null or empty");
-        }
-        
-        Group expected = new Group();
-        expected.setName("It is Group"+new Date().getTime());
-        expected.setDescription("It is description");
-        expected.setLeader(leader);
-        
-        Group resultGroup = groupController.makingGroup(expected);
-        
-        Assert.assertEquals(expected.getName(), resultGroup.getName());
-    }
-    
-    @Test
-    public void findGroupById(){
-        Group expected = groupController.findGroupAll().get(0);
-        Group result = groupController.findGroupById(expected.getId());
-        Assert.assertEquals(expected.getId(), result.getId());
-        
-    }
-    
-    @Test
-    public void findGroupByName(){
-        List<Group> groups = groupController.findGroupAll();
-        Group expected = groups.get(0);
-        Group result = groupController.findGroupByName(expected.getName());
-        
-        Assert.assertEquals(expected.getName(), result.getName()); 
-        
-    }
-    
-    @Test
-    public void findGroupAll() {
-        List<Group> groups = groupController.findGroupAll();
-        Assert.assertNotNull(groups);
-        Assert.assertFalse(groups.isEmpty());
-    }
-    
+	@Autowired
+	private GroupController groupController;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private MemberMapper memberMapper;
+
+	@Before
+	public void setUp() {
+		DBUnitOperator.setDataSource(dataSource);
+		IDataSet dataset = IDataSetFactory.fromXml("src/test/resources/testdataset/groupt_test_setup.xml");
+		DBUnitOperator.cleanInsert(dataset);
+	}
+
+	@Test
+	public void findGroupsJoined() throws Exception {
+		Member member = new Member();
+		member.setId(1);
+
+		List<Group> joinedGroups = groupController.findGroupsJoined(member);
+		
+		Assert.assertTrue(joinedGroups.size() == 1);
+		Assert.assertTrue(joinedGroups.get(0).getId() == 1);
+	}
+
+	@Test
+	public void createGroup() {
+		Member leader = memberMapper.selectMemberByEmail("arst@arst.com");
+
+		Group expected = new Group();
+		expected.setName("It is Group" + new Date().getTime());
+		expected.setDescription("It is description");
+		expected.setLeader(leader);
+
+		Group actual = groupController.makingGroup(expected);
+
+		Assert.assertEquals(expected.getName(), actual.getName());
+		Assert.assertEquals(expected.getDescription(), actual.getDescription());
+		Assert.assertEquals(expected.getLeader(), actual.getLeader());
+	}
+
+	@Test
+	public void findGroupById() {
+		Group expected = groupController.findGroupAll().get(0);
+		Group actual = groupController.findGroupById(expected.getId());
+		
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void findGroupByName() {
+		Group expected = groupController.findGroupAll().get(0);
+		Group actual = groupController.findGroupByName(expected.getName());
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void findGroupAll() {
+		List<Group> groups = groupController.findGroupAll();
+		
+		Assert.assertTrue(groups.size() > 0);
+	}
+
 }
