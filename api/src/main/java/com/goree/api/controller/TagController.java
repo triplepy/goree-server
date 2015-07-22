73f181f126a1a301ba3740a6c72dd156350a920d
kyunooh <chm073@gmail.com>
2015-07-23 00:40:12 +0900

    Updating @Controller to @RestController

--- api/src/main/java/com/goree/api/controller/TagController.java
+++ api/src/main/java/com/goree/api/controller/TagController.java
@@ -3,12 +3,12 @@ package com.goree.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.goree.api.domain.Tag;
 import com.goree.api.service.TagService;
 
-@Controller
+@RestController
 public class TagController {
 
 	@Autowired
