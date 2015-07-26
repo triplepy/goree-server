06767af922ebd90a8f3b3d8ad97e9afec075c6ce
WonyoungJu <getogrand@gmail.com>
2015-07-26 22:08:36 +0900

    implementation of retrieving token.

--- api/src/test/java/com/goree/api/auth/FacebookTest.java
+++ api/src/test/java/com/goree/api/auth/FacebookTest.java
@@ -1,19 +1,24 @@
 package com.goree.api.auth;
 
+import com.goree.api.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.goree.api.Application;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={Application.class})
+@WebAppConfiguration
 public class FacebookTest {
     @Autowired
     private WebApplicationContext webContext;
@@ -26,9 +31,10 @@ public class FacebookTest {
     }
     
     @Test
-    public void retrieveShortLivedToken() {
-        this.mockMvc.perform(get("/short-lived-token/test112312312423432")
-                .andExpect(status().isOk()))
-                .andExpect(content)
+    public void retrieveShortLivedToken() throws Exception {
+        String shortLivedToken = "test112312312423432";
+        this.mockMvc.perform(get("/short-lived-token/" + shortLivedToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(shortLivedToken));
     }
 }
