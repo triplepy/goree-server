20ebc69b299a6ee4f789b6d05640c7fb1339c934
WonyoungJu <getogrand@gmail.com>
2015-07-26 21:35:46 +0900

    refactor equals method of Note

--- api/src/main/java/com/goree/api/domain/Note.java
+++ api/src/main/java/com/goree/api/domain/Note.java
@@ -1,35 +1,20 @@
 package com.goree.api.domain;
 
-import java.util.Date;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(of={"id", "content"})
 public class Note {
     private int id;
     private Group group;
     private Member noteWriter;
     private String content;
     private Date createDate;
-
-    public boolean is (Note other) {
-        return this.id == other.id;
-    }
-
-//    public boolean equals(Object other) {
-//        if (! (other instanceof Note))
-//            return false;
-//        
-//        Note note = (Note) other;
-//        return group.equals(note.getGroup())
-//                && noteWriter.equals(note.getNoteWriter())
-//                && content.equals(note.getContent())
-//                && createDate.equals(note.getCreateDate());
-//    }
 }
--- api/src/test/java/com/goree/api/controller/NoteTest.java
+++ api/src/test/java/com/goree/api/controller/NoteTest.java
@@ -4,11 +4,9 @@ import com.goree.api.domain.Group;
 import com.goree.api.domain.Member;
 import com.goree.api.domain.Note;
 import com.goree.api.util.TestWithDBUnit;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestExecutionListeners;
 
 
 public class NoteTest extends TestWithDBUnit{
@@ -39,28 +37,7 @@ public class NoteTest extends TestWithDBUnit{
         expected.setContent("findNoteById");
         Note actual = noteController.findNoteById(expected.getId());
 
-
-        Assert.assertTrue(expected.is(actual));
-        Assert.assertEquals(expected.getContent(),actual.getContent());
-        Assert.assertEquals(expected.getGroup().getId(), actual.getGroup().getId());
-        Assert.assertEquals(expected.getNoteWriter().getId(), actual.getNoteWriter().getId());
-        Assert.assertNotNull(actual.getCreateDate());
-
-
+        Assert.assertEquals(expected, actual);
     }
-
-    @Test
-    public void is(){
-        Note note1 = new Note();
-        note1.setId(100);
-        Note note2 = new Note();
-        note2.setId(100);
-        Note note3 = new Note();
-        note3.setId(4);
-
-        Assert.assertTrue(note1.is(note2));
-        Assert.assertFalse(note1.is(note3));
-    }
-
 
 }
