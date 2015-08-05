545886037985b305919ee5d39249a562dd0f4d85
JellyChoi <chm073@gmail.com>
2015-08-05 13:56:19 +0900

    MemberTest to MemberServiceTest

--- api/src/test/java/com/goree/api/service/MemberServiceTest.java
+++ api/src/test/java/com/goree/api/service/MemberServiceTest.java
@@ -1,10 +1,9 @@
-package com.goree.api.controller;
+package com.goree.api.service;
 
 import com.goree.api.domain.Group;
 import com.goree.api.domain.Member;
 import com.goree.api.domain.Member.Gender;
 import com.goree.api.domain.Tag;
-import com.goree.api.service.MemberService;
 import com.goree.api.util.TestWithDBUnit;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,17 +18,15 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 
-public class MemberTest extends TestWithDBUnit{
-
-    @Autowired
-    private MemberController memberController;
-    @Autowired
-    private GroupController groupController;
-    
-    private Member testMember;
+public class MemberServiceTest extends TestWithDBUnit{
 
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private GroupService groupService;
+    
+    private Member testMember;
+
 
     @Override
     public String getDatasetFilePath() {
@@ -40,12 +37,12 @@ public class MemberTest extends TestWithDBUnit{
     @Before
     public void setUp() {
         super.setUp();
-        testMember = memberController.findMemberAll().get(0);
+        testMember = memberService.findMemberAll().get(0);
     }
     
     @Test
     public void findMemberAll() {
-        List<Member> members = memberController.findMemberAll();
+        List<Member> members = memberService.findMemberAll();
         Assert.assertNotNull(members);
         Assert.assertFalse(members.isEmpty());
     }
@@ -53,7 +50,7 @@ public class MemberTest extends TestWithDBUnit{
     @Test 
     public void registerMember() {
         Member expected = new Member();
-        expected.setEmail("rpxhdnjsdud"+new Date().getTime()+"@nate.com");
+        expected.setEmail("rpxhdnjsdud" + new Date().getTime() + "@nate.com");
         expected.setPassword("qlalfqjsgh");
         expected.setFullName("Wonyoung Ju");
         expected.setAge(22);
@@ -64,31 +61,31 @@ public class MemberTest extends TestWithDBUnit{
         Tag tag = new Tag();
         tag.setName("memberTest");
         expected.setTags(Arrays.asList(tag));
-        Member registered = memberController.registerMember(expected);
+        Member registered = memberService.registerMember(expected);
         Assert.assertNotNull(registered);
         Assert.assertEquals(expected.getEmail(), registered.getEmail());
     }
     
     @Test
     public void deleteMemberById() {
-        memberController.deleteMemberById(testMember.getId());
+        memberService.deleteMemberById(testMember.getId());
         
-        boolean deleted = !memberController.findMemberAll().stream()
+        boolean deleted = !memberService.findMemberAll().stream()
                 .anyMatch(m-> m.getId() == testMember.getId());
         Assert.assertTrue(deleted);
         
         List<Group> groupsHasDeletedMember = 
-                groupController.findGroupsJoined(testMember);
+                groupService.findRegistedGroupsByMember(testMember);
         Assert.assertTrue(groupsHasDeletedMember.isEmpty());
     }
     
     @Test
     public void joinMemberToGroup() {
-        Group toBeJoined = groupController.findGroupAll().get(0);
+        Group toBeJoined = groupService.findGroupAll().get(0);
         long toBeJoinedGroupId = toBeJoined.getId();
-        groupController.joinMember(toBeJoinedGroupId, testMember.getId());
+        groupService.joinMember(toBeJoinedGroupId, testMember.getId());
         
-        List<Group> joinedGroups = groupController.findGroupsJoined(testMember);
+        List<Group> joinedGroups = groupService.findRegistedGroupsByMember(testMember);
         boolean joined = joinedGroups.contains(toBeJoined);
         Assert.assertTrue(joined);
     }
@@ -103,7 +100,7 @@ public class MemberTest extends TestWithDBUnit{
         expected.setAge(20);
         expected.setNickname("arstarst");
         expected.setGender(Gender.M);
-        Member actual = memberController.findMemberById(expected.getId());
+        Member actual = memberService.findMemberById(expected.getId());
 
         Assert.assertEquals(expected.getEmail(), actual.getEmail());
         Assert.assertEquals(expected.getNickname(), actual.getNickname());
@@ -111,7 +108,7 @@ public class MemberTest extends TestWithDBUnit{
 
     @Test
     public void findMemberByFacebookUserId() {
-        Member expected = memberController.findMemberById(1L);
+        Member expected = memberService.findMemberById(1L);
         Member actual = memberService.findMemberByFacebookUserId("12345");
 
         assertThat(actual.getId(), is(expected.getId()));
