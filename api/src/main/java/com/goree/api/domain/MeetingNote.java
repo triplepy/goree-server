2669d49a6b8ff436e7edf7b61040c63f1f55f162
kyunooh <chm073@gmail.com>
2015-08-17 19:56:45 +0900

    Add ToString

--- api/src/main/java/com/goree/api/domain/MeetingNote.java
+++ api/src/main/java/com/goree/api/domain/MeetingNote.java
@@ -2,11 +2,13 @@ package com.goree.api.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@ToString
 public class MeetingNote {
     private long id;
     private Meeting meeting;
