fac6f0e26e2a9d51d3d4e4c1598e5256eac57d61
WonyoungJu <getogrand@gmail.com>
2015-08-21 22:01:33 +0900

    Just See

--- api/src/main/java/com/goree/api/service/MeetingNoteService.java
+++ api/src/main/java/com/goree/api/service/MeetingNoteService.java
@@ -6,12 +6,21 @@ import com.goree.api.mapper.MeetingNoteMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.google.common.base.Preconditions.*;
+import static java.util.Objects.*;
+import static org.apache.commons.lang3.StringUtils.*;
+
 @Service
 public class MeetingNoteService {
     @Autowired
     private MeetingNoteMapper meetingNoteMapper;
 
     public MeetingNote writeMeetingNote(MeetingNote meetingNote) {
+        checkArgument(meetingNote != null);
+        requireNonNull(meetingNote.getMeeting());
+        requireNonNull(meetingNote.getWriter());
+        checkArgument(isNotBlank(meetingNote.getContent()));
+
         meetingNoteMapper.insertMeetingNote(meetingNote);
         return findLastMeetingNoteByMeetingId(meetingNote.getMeeting().getId());
     }
