43255ca95066c7ea91188968f0039ea0697a476b
WonyoungJu <getogrand@gmail.com>
2015-08-29 21:47:56 +0900

    createPlace(), findPlaceByItself() 테스트코드 작성

--- api/src/main/java/com/goree/api/controller/PlaceController.java
+++ api/src/main/java/com/goree/api/controller/PlaceController.java
@@ -3,15 +3,15 @@ package com.goree.api.controller;
 import com.goree.api.domain.Place;
 import com.goree.api.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PlaceController {
     private static final String URL_PREFIX = "/place";
-    public static final String FIND_PLACE_BY_ID = URL_PREFIX + "/{id}";
+    public static final String FIND_PLACE_BY_ID_URL = URL_PREFIX + "/{id}";
+    public static final String CREATE_PLACE_URL = URL_PREFIX;
+    public static final String FIND_PLACE_BY_ITSELF = URL_PREFIX + "/itself";
+
     @Autowired
     private PlaceService placeService;
 
@@ -20,7 +20,7 @@ public class PlaceController {
      * @apiGroup
      * @apiDescription
      */
-    @RequestMapping(value = FIND_PLACE_BY_ID, method = RequestMethod.GET)
+    @RequestMapping(value = FIND_PLACE_BY_ID_URL, method = RequestMethod.GET)
     public Place findPlaceById(@PathVariable long id) {
         return placeService.findPlaceById(id);
     }
@@ -30,7 +30,8 @@ public class PlaceController {
      * @apiGroup
      * @apiDescription
      */
-    public Place createPlace(Place place) {
+    @RequestMapping(value = CREATE_PLACE_URL, method = RequestMethod.POST)
+    public Place createPlace(@RequestBody Place place) {
         return placeService.createPlace(place);
     }
 
@@ -39,7 +40,8 @@ public class PlaceController {
      * @apiGroup
      * @apiDescription
      */
-    public Place findPlaceByItself(Place place) {
+    @RequestMapping(value = FIND_PLACE_BY_ITSELF, method = RequestMethod.GET)
+    public Place findPlaceByItself(@RequestBody Place place) {
         return placeService.findPlaceByItself(place);
     }
     
--- api/src/test/java/com/goree/api/controller/PlaceControllerTest.java
+++ api/src/test/java/com/goree/api/controller/PlaceControllerTest.java
@@ -1,11 +1,13 @@
 package com.goree.api.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goree.api.Application;
 import com.goree.api.auth.FacebookSettings;
 import com.goree.api.domain.Place;
 import com.goree.api.service.PlaceService;
 import com.goree.api.util.HttpHeaderConstants;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +27,6 @@ import java.math.BigDecimal;
 import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -62,9 +64,65 @@ public class PlaceControllerTest {
 
         // when then
         mockMvc.perform(
-                get(PlaceController.FIND_PLACE_BY_ID, expected.getId())
+                get(PlaceController.FIND_PLACE_BY_ID_URL, expected.getId())
                         .header(HttpHeaderConstants.AUTH_TOKEN, settings.longLivedTokenForTest()))
-                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is((int)expected.getId())))
+                .andExpect(jsonPath("$.name", is(expected.getName())))
+                .andExpect(jsonPath("$.address", is(expected.getAddress())))
+                .andExpect(jsonPath("$.xCoordinate", is(expected.getXCoordinate().doubleValue())))
+                .andExpect(jsonPath("$.yCoordinate", is(expected.getYCoordinate().doubleValue())));
+    }
+
+    @Test
+    public void createPlace() throws Exception {
+        // given
+        Place newPlace = new Place();
+        newPlace.setName("goodnews");
+        newPlace.setAddress("서울시 용산구 청파동 선린인고 앞");
+        newPlace.setXCoordinate(new BigDecimal("36.017194"));
+        newPlace.setYCoordinate(new BigDecimal("128.6978236"));
+
+        Place created = new Place();
+        PropertyUtils.copyProperties(created, newPlace);
+        created.setId(1L);
+
+        when(placeService.createPlace(newPlace)).thenReturn(created);
+
+        // when then
+        mockMvc.perform(
+                post(PlaceController.CREATE_PLACE_URL)
+                        .header(HttpHeaderConstants.AUTH_TOKEN, settings.longLivedTokenForTest())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(newPlace)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is((int)created.getId())))
+                .andExpect(jsonPath("$.name", is(created.getName())))
+                .andExpect(jsonPath("$.address", is(created.getAddress())))
+                .andExpect(jsonPath("$.xCoordinate", is(created.getXCoordinate().doubleValue())))
+                .andExpect(jsonPath("$.yCoordinate", is(created.getYCoordinate().doubleValue())));
+    }
+
+    @Test
+    public void findPlaceByItself() throws Exception {
+        // given
+        Place searchCondition = new Place();
+        searchCondition.setAddress("서울시 용산구 청파동 선린인고 앞");
+
+        Place expected = new Place();
+        expected.setName("goodnews");
+        expected.setAddress("서울시 용산구 청파동 선린인고 앞");
+        expected.setXCoordinate(new BigDecimal("36.017194"));
+        expected.setYCoordinate(new BigDecimal("128.6978236"));
+
+        when(placeService.findPlaceByItself(searchCondition)).thenReturn(expected);
+
+        // when then
+        mockMvc.perform(
+                get(PlaceController.FIND_PLACE_BY_ITSELF)
+                        .header(HttpHeaderConstants.AUTH_TOKEN, settings.longLivedTokenForTest())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(searchCondition)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is((int)expected.getId())))
                 .andExpect(jsonPath("$.name", is(expected.getName())))
