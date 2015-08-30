0f4fed68f5a9cf53e273603d2a34075290721941
kyunooh <chm073@gmail.com>
2015-08-31 01:29:01 +0900

    Optimize more

--- api/src/test/java/com/goree/api/util/DBUnitOperator.java
+++ api/src/test/java/com/goree/api/util/DBUnitOperator.java
@@ -1,9 +1,5 @@
 package com.goree.api.util;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.dbunit.DataSourceDatabaseTester;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.database.IDatabaseConnection;
@@ -12,40 +8,43 @@ import org.dbunit.operation.DatabaseOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @Component
 public class DBUnitOperator {
-    private IDatabaseTester tester;
-    
+    private static IDatabaseTester tester;
+
     @Autowired
-    public DBUnitOperator(DataSource dataSource) {
-        this.tester = new DataSourceDatabaseTester(dataSource); 
+    private DBUnitOperator(DataSource dataSource) {
+        this.tester = new DataSourceDatabaseTester(dataSource);
     }
     
-    public void cleanInsert(IDataSet dataset) {
+    public static final void cleanInsert(IDataSet dataset) {
         execute(DatabaseOperation.CLEAN_INSERT, dataset);
     }
-    
-    public void delete(IDataSet dataset) {
+
+    public static final void delete(IDataSet dataset) {
         execute(DatabaseOperation.DELETE, dataset);
     }
-    
-    public void deleteAll(IDataSet dataset) {
+
+    public static final void deleteAll(IDataSet dataset) {
         execute(DatabaseOperation.DELETE_ALL, dataset);
     }
-    
-    public void insert(IDataSet dataset) {
+
+    public static final void insert(IDataSet dataset) {
         execute(DatabaseOperation.INSERT, dataset);
     }
-    
-    public void refresh(IDataSet dataset) {
+
+    public static final void refresh(IDataSet dataset) {
         execute(DatabaseOperation.REFRESH, dataset);
     }
-    
-    public void truncateTable(IDataSet dataset) {
+
+    public static final void truncateTable(IDataSet dataset) {
         execute(DatabaseOperation.TRUNCATE_TABLE, dataset);
     }
     
-    private void execute(DatabaseOperation operation, IDataSet dataset) {
+    private static final void execute(DatabaseOperation operation, IDataSet dataset) {
         IDatabaseConnection conn = null;
         try {
             conn = tester.getConnection();
--- api/src/test/java/com/goree/api/util/TestWithDBUnit.java
+++ api/src/test/java/com/goree/api/util/TestWithDBUnit.java
@@ -5,26 +5,22 @@ import org.dbunit.dataset.IDataSet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={Application.class})
 public abstract class TestWithDBUnit {
-    @Autowired
-    private DBUnitOperator operator;
-
 	private final IDataSet dataset = IDataSetFactory.fromXml(getDatasetFilePath());;
 
 	@Before
 	public void setUp() {
-		operator.cleanInsert(dataset);
+		DBUnitOperator.cleanInsert(dataset);
 	}
 	
 	@After
 	public void tearDown() {
-        operator.deleteAll(dataset);
+		DBUnitOperator.deleteAll(dataset);
 	}
 	
 	protected abstract String getDatasetFilePath();
